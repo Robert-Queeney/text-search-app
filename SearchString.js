3c09b8958ee08544ef64cf5c2911fea36d4fca67
Robert Queeney <32365387+Robert-Queeney@users.noreply.github.com>
2019-10-31 14:12:25 -0500

    Finished JS logic

--- SearchString.js
+++ SearchString.js
@@ -42,10 +42,16 @@ function getDocStats(fileContent){
     let text = fileContent.toLowerCase();
     let wordArray = text.match(/\b\S+\b/g);  //returns an array of words
     let wordDictionary = {}; //creating an object that olds multiple key value pairs in order to count the amount
+    console.log(wordArray)
+    var uncommonWords = [];
+
+    //filter out the uncommon words
+    uncommonWords = filterStopWords(wordArray);
+
 
     //count every word in the array
-    for( let word in wordArray){
-        let wordValue = wordArray[word];
+    for( let word in uncommonWords){
+        let wordValue = uncommonWords[word];
         if (wordDictionary[wordValue] > 0){
             wordDictionary[wordValue] += 1; 
         } else {
@@ -63,7 +69,10 @@ function getDocStats(fileContent){
 
     //wriet them to the page
     ULTemplate(top5Words, document.getElementById("mostUsed"));
-    ULTemplate(least5Words, document.getElementById("leastused"));
+    ULTemplate(least5Words, document.getElementById("leastUsed"));
+
+    docLength.innerText = "Document Length: " + text.length; //length of docuemnt
+    wordCount.innerText = "Word Count: " + wordArray.length; 
 
 }
 
@@ -72,8 +81,8 @@ function ULTemplate(items, element){
     let templateHTML = rowTemplate.innerHTML;
     let resultsHTML = "";
 
-    for(i-0; i < items.length-1; i++){
-        resultsHTML += templateHTML.replace('{{val}}', items[i][0] + " : " + items[i][1] + "time(s)");
+    for(i=0; i < items.length-1; i++){
+        resultsHTML += templateHTML.replace('{{val}}', items[i][0] + " : " + items[i][1] + " time(s)");
     }
 
     element.innerHTML = resultsHTML; 
@@ -81,7 +90,7 @@ function ULTemplate(items, element){
 
 function sortProperties(obj){
     //1. return the obj as an array
-    let rtnArray = Object.defineProperties(obj);
+    let rtnArray = Object.entries(obj);
 
     //2. sort it
     rtnArray.sort(function(first, second){
@@ -89,4 +98,63 @@ function sortProperties(obj){
     }); 
 
     return rtnArray; 
+}
+
+//filter out stop words
+const filterStopWords = (wordArray) => {
+    let commonWords = getStopWords();
+    let commonObject = {};
+    let uncommonArr = [];
+
+    for(i=0; i<commonWords.length; i++){
+        commonObject[commonWords[i].trim()] = true; 
+    }
+
+    for(i=0; i < wordArray.length; i ++){
+        word = wordArray[i].trim().toLowerCase();
+        if(!commonObject[word]){
+            uncommonArr.push(word);
+        }
+    }
+
+    return uncommonArr;
+
+}
+
+const getStopWords = () => {
+    return ["a", "it'd", "out", "up", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"];
+} 
+
+//inserts HTMl into the document to mark the highlighted words in search
+const performMark = () => {
+    
+    //read the keyword
+    let keyword = document.getElementById("keyword").value; 
+    let display = document.getElementById("fileContent");
+
+    let newContent = " ";
+
+    // find all the currently marked items
+    let spans = document.querySelectorAll('mark');  // <mark> </mark>  this is what it will look for
+    
+    for (let i = 0; i < spans.length;  i++){
+        spans[i].outerHTML = spans[i].innerHTML; // outer is the html and the content   inner is just the content  ie:  turns <mark> Harry </mark> into Harry
+    }
+
+    let re = new RegExp(keyword, "gi" );   //"gi" means globally and case insensitive
+    let replaceText = "<mark id='markme'>$&</mark>"; //the $& allows the current content to go into the middle 
+    let bookContent = display.innerHTML; 
+
+    // add the mark to the book content
+    newContent = bookContent.replace(re, replaceText); 
+
+    display.innerHTML = newContent;
+    let count = document.querySelectorAll('mark').length; 
+    document.getElementById("searchstat").innerHTML = "found " + count + " matches";
+
+    if(count > 0) {
+        let element = document.getElementById("markme");
+        element.scrollIntoView();
+    }
+
 }
